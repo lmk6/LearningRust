7ec20ef6716dfc613abe711733593e623036e590
Lukasz Kruszynski <kruluk01@gmail.com>
2024-05-19 22:37:49 +0100

    Encapsulated Database controller + new functionalities

--- src/db_controller.rs
+++ src/db_controller.rs
@@ -1,57 +1,76 @@
 use rusqlite::{params, Connection, Result};
 use std::collections::HashMap;
 
-pub fn initialise_database(connection: &Connection) -> Result<()> {
-    connection.execute(
-        "CREATE TABLE IF NOT EXISTS entries (\
+pub struct Database {
+    connection: Connection,
+}
+
+impl Database {
+    pub fn new(database_path: &str) -> Result<Self> {
+        let connection = Connection::open(database_path)?;
+        Ok(Database { connection })
+    }
+
+    pub fn initialise_database(&self) -> Result<()> {
+        self.connection.execute(
+            "CREATE TABLE IF NOT EXISTS entries (\
                 key TEXT PRIMARY KEY,\
                 value INTEGER\
                 )",
-        (),
-    )?;
-    let mut statement = connection.prepare("SELECT COUNT(*) FROM entries")?;
-    let count: i32 = statement.query_row([], |row| row.get(0))?;
+            (),
+        )?;
+        let mut statement = self.connection.prepare("SELECT COUNT(*) FROM entries")?;
+        let count: i32 = statement.query_row([], |row| row.get(0))?;
 
-    if count == 0 {
-        fill_db(connection)?;
+        if count == 0 {
+            self.fill_db()?;
+        }
+
+        Ok(())
     }
 
-    Ok(())
-}
+    fn fill_db(&self) -> Result<()> {
+        let mut hashmap: HashMap<String, i32> = HashMap::new();
+        hashmap.insert("apple".to_string(), 10);
+        hashmap.insert("banana".to_string(), 20);
+        hashmap.insert("orange".to_string(), 15);
 
-fn fill_db(connection: &Connection) -> Result<()> {
-    let mut hashmap: HashMap<String, i32> = HashMap::new();
-    hashmap.insert("apple".to_string(), 10);
-    hashmap.insert("banana".to_string(), 20);
-    hashmap.insert("orange".to_string(), 15);
+        for (key, value) in hashmap {
+            self.connection.execute(
+                "INSERT INTO entries (key, value) VALUES (?1, ?2)",
+                params![key, value],
+            )?;
+        }
+        Ok(())
+    }
 
-    for (key, value) in hashmap {
-        connection.execute(
-            "INSERT INTO entries (key, value) VALUES (?1, ?2)",
+    pub fn load_hashmap_from_db(&self, hash_map: &mut HashMap<String, i32>) -> Result<()> {
+        let mut statement = self.connection.prepare("SELECT key, value FROM entries")?;
+        let iterator = statement.query_map([], |row| {
+            Ok((row.get::<_, String>(0)?, row.get::<_, i32>(1)?))
+        })?;
+
+        for entry in iterator {
+            let (key, value): (String, i32) = entry?;
+            hash_map.insert(key, value);
+        }
+        Ok(())
+    }
+
+    pub fn add_entry_to_db(&self, key: &str, value: i32) -> Result<()> {
+        self.connection.execute(
+            "INSERT INTO entries (key, value) VALUES (?1, ?2)\
+            ON CONFLICT(key) DO UPDATE SET value = excluded.value",
             params![key, value],
         )?;
+        Ok(())
     }
-    Ok(())
-}
 
-pub fn load_hashmap_from_db(connection: &Connection, hash_map: &mut HashMap<String, i32>) -> Result<()> {
-    let mut statement = connection.prepare("SELECT key, value FROM entries")?;
-    let iterator = statement.query_map([], |row| {
-        Ok((row.get::<_, String>(0)?, row.get::<_, i32>(1)?))
-    })?;
-
-    for entry in iterator {
-        let (key, value): (String, i32) = entry?;
-        hash_map.insert(key, value);
+    pub fn remove_entry_by_key(&self, key: &str) -> Result<()> {
+        self.connection.execute(
+            "DELETE FROM entries WHERE key = ?1",
+            params![key]
+        )?;
+        Ok(())
     }
-    Ok(())
-}
-
-pub fn add_entry_to_db(connection: Connection, key: &str, value: i32) -> Result<()> {
-    connection.execute(
-        "INSERT INTO entries (key, value) VALUES (?1, ?2)\
-            ON CONFLICT(key) DO UPDATE SET value = excluded.value",
-        params![key, value],
-    )?;
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,63 +3,68 @@ mod db_controller;
 use std::collections::HashMap;
 use std::io;
 use std::io::Write;
-use rusqlite::Connection;
-use crate::db_controller::{initialise_database, load_hashmap_from_db};
+use crate::db_controller::Database;
 
 fn main() -> rusqlite::Result<()> {
-    let connection = Connection::open("database.db")?;
-    initialise_database(&connection)?;
+    let db = Database::new("my_database.db")?;
+    db.initialise_database()?;
 
-    let mut hashmap: HashMap<String, i32> = HashMap::new();
-    load_hashmap_from_db(&connection, &mut hashmap)?;
-
-    menu_sel(&mut hashmap);
+    menu_sel(&db);
     Ok(())
 }
 
-fn menu_sel(map: &mut HashMap<String, i32>) {
+fn menu_sel(db: &Database) {
     loop {
         println!("1. Add new entry");
         println!("2. Show all entries");
+        println!("3. Remove entry");
         io::stdout().flush().unwrap();
         let choice = get_int_input_loop();
         match choice {
-            1 => add_new_to_hashmap(map),
-            2 => print_hashmap(map),
+            1 => add_new_entry(&db),
+            2 => print_all_entries(&db),
+            3 => remove_entry(&db),
             _ => println!("Wrong choice!")
         }
     }
 }
 
-fn add_new_to_hashmap(map: &mut HashMap<String, i32>) {
+fn add_new_entry(db: &Database) {
     let mut new_key = String::new();
-    fill_str_input_loop(&mut new_key);
+    fill_str_input_loop(&mut new_key, "Enter the new key: ");
     let new_value = get_int_input_loop();
-    map.entry(new_key)
-        .and_modify(|v| *v = new_value)
-        .or_insert(new_value);
-}
-
-fn print_hashmap(map: &mut HashMap<String, i32>) {
-    println!("Hash Map's contents are:");
-    for (key, value) in map {
-        println!("{}: {}", key, value);
+    match db.add_entry_to_db(&new_key, new_value) {
+        Ok(()) => println!("New Entry Created!"),
+        Err(err) => println!("Error: {}", err),
     }
 }
 
-fn fill_str_input_loop(parse_str: &mut String) {
+fn print_all_entries(db: &Database) {
+    let mut map: HashMap<String, i32> = HashMap::new();
+    match db.load_hashmap_from_db(&mut map) {
+        Ok(()) => {
+            println!("Hash Map's contents are:");
+            for (key, value) in map {
+                println!("{}: {}", key, value);
+            }
+        },
+        Err(error) => println!("Error Encountered: {}", error),
+    };
+}
+
+fn fill_str_input_loop(parse_str: &mut String, input_msg: &str) {
     loop {
-        if let Some(input) = get_str_input() {
+        if let Some(input) = get_str_input(&input_msg) {
             parse_str.push_str(&input);
             break;
         }
     }
 }
 
-fn get_str_input() -> Option<String> {
+fn get_str_input(input_msg: &str) -> Option<String> {
     let mut input = String::new();
 
-    print!("Enter a string: ");
+    print!("{}", input_msg);
     io::stdout().flush().unwrap();
 
     io::stdin().read_line(&mut input).expect("Failed to read line");
@@ -73,21 +78,32 @@ fn get_str_input() -> Option<String> {
     }
 }
 
+fn remove_entry(db: &Database) {
+    print_all_entries(&db);
+    let mut key = String::new();
+    fill_str_input_loop(&mut key, "Enter key to delete: ");
+
+    match db.remove_entry_by_key(&key) {
+        Ok(()) => println!("Deletion Successful!"),
+        Err(err) => println!("Deletion Failed: {}", err),
+    }
+}
+
 
 fn get_int_input_loop() -> i32 {
     let mut input = None;
 
     while input.is_none() {
-        input = get_int_input();
+        input = get_int_input("Enter a number: ");
     }
 
     input.unwrap()
 }
 
-fn get_int_input() -> Option<i32> {
+fn get_int_input(input_msg: &str) -> Option<i32> {
     let mut input = String::new();
 
-    print!("Enter a number: ");
+    print!("{}", input_msg);
     io::stdout().flush().unwrap();
 
     io::stdin().read_line(&mut input).expect("Failed to read line");
